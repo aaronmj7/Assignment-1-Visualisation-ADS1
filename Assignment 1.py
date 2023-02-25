b8cbe4078af7a89a1d1518f017b544ebd9083aa6
Aaron M Joseph <125889154+aaronmj7@users.noreply.github.com>
2023-02-25 17:21:01 +0000

    Add files via upload

--- Assignment 1.py
+++ Assignment 1.py
@@ -5,10 +5,14 @@ Created on Fri Feb 24 08:58:00 2023
 @author: aaron
 """
 
-import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+unemp_rate = pd.read_excel("g7_unemployment_rates.xlsx", index_col=0)
+cpi = pd.read_excel("g7_cpi.xlsx", index_col=0)
 
+plt.figure()
+plt.plot(unemp_rate.index,unemp_rate["Canada"])
+plt.xlim(2001,2020)
+plt.show()
 
-75rig
