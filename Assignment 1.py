5d7ab36e88943babce86be2a854b5704359b9c83
Aaron M Joseph <125889154+aaronmj7@users.noreply.github.com>
2023-02-25 18:34:55 +0000

    Add files via upload

--- Assignment 1.py
+++ Assignment 1.py
@@ -8,11 +8,29 @@ Created on Fri Feb 24 08:58:00 2023
 import pandas as pd
 import matplotlib.pyplot as plt
 
+
+def lineplot(list):
+    """ This Function plots the list as line plot
+    using matplotlib.pyplot
+    """
+    plt.figure(figsize=(13,5.75))
+
+    for i in list:
+        plt.plot(unemp_rate.index,unemp_rate[i],label=i)
+
+    plt.xticks(range(2001,2021))
+    plt.xlim(2001,2020)
+    plt.xlabel("Year")
+    plt.ylabel("Unemployment Rate")
+    plt.title("Unemployment Rates of G7 Countries(2001-2020)")
+    plt.legend()
+    plt.show()
+
+
 unemp_rate = pd.read_excel("g7_unemployment_rates.xlsx", index_col=0)
 cpi = pd.read_excel("g7_cpi.xlsx", index_col=0)
 
-plt.figure()
-plt.plot(unemp_rate.index,unemp_rate["Canada"])
-plt.xlim(2001,2020)
-plt.show()
+countries = ["Canada","France","Germany","Italy","Japan",
+             "United Kingdom","United States"]
+lineplot(countries)
 
